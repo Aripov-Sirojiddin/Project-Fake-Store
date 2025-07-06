8f869549ae7b46d11e9cbcab01ce3fa1719e1c44
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-07-06 17:59:04 +0500

    Added a search bar which creates a new param and sets it to the value of the search text.

--- src/componets/navbar/navbar.jsx
+++ src/componets/navbar/navbar.jsx
@@ -1,8 +1,34 @@
+import { Form, useLocation, useSubmit } from "react-router-dom";
 import styles from "./navbar.module.css";
 export default function Navbar({}) {
+  const location = useLocation();
+  const submit = useSubmit();
+
+  function handleSearch(e) {
+    const params = new URLSearchParams(location.search);
+    console.log(params);
+
+    const inputText = e.target.value;
+    if (inputText !== "") {
+      params.set("search", inputText);
+    } else {
+      params.delete("search");
+    }
+    
+    submit(params);
+  }
   return (
     <div className={styles.container}>
       <p>Navbar</p>
+      <Form>
+        <input
+          type="text"
+          id="search"
+          name="search"
+          onChange={handleSearch}
+          placeholder="Looking for something specific?"
+        />
+      </Form>
     </div>
   );
 }
