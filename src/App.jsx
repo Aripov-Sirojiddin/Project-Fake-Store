46956685a2dd3a26260c2ee980be2517a58148a7
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-07-06 17:14:49 +0500

    In the sidebar form added a select for categories.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./componets/navbar/navbar";
 import Sidebar from "./componets/sidebar/sidebar";
 import Footer from "./componets/footer/footer";
+import { useSubmit } from "react-router-dom";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const submit = useSubmit();
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((result) => {
+        if (!result.ok) {
+          throw Error(
+            `Error has occured when pulling from API: ${result.status}`
+          );
+        }
+        return result.json();
+      })
+      .then((data) => {
+        setProducts(data);
+
+        const categoriesSet = new Set([]);
+        for (let i = 0; i < data.length; i++) {
+          if (!categoriesSet.has(data[i].category)) {
+            categoriesSet.add(data[i].category);
+          }
+        }
+        const categoriesArray = [...categoriesSet];
+        submit(categoriesArray[0]);
+        setCategories(categoriesArray);
+      });
+  }, []);
+
   return (
     <>
       <Navbar />
       <div className="main">
-        <Sidebar/>
+        <Sidebar categories={categories} />
         <div className="content">
           <p>Hello</p>
         </div>
--- src/componets/sidebar/sidebar.jsx
+++ src/componets/sidebar/sidebar.jsx
@@ -1,9 +1,36 @@
-import styles from "./sidebar.module.css"
+import { Form, useSubmit } from "react-router-dom";
+import styles from "./sidebar.module.css";
 
-export default function Sidebar({}) {
+export default function Sidebar(props) {
+  const submit = useSubmit();
+
+  const categories = props.categories
+    ? props.categories
+    : ["men's clothing", "jewelry", "electronics", "women's clothing"];
+
+  const options = [];
+
+  for (let i = 0; i < categories.length; i++) {
+    options.push(
+      <option key={categories[i]} value={categories[i]}>
+        {categories[i]}
+      </option>
+    );
+  }
+  
+  function handleSelectCategory(e) {
+    submit(e.target.value);
+  }
+  
   return (
     <div className={styles.container}>
-      <p>Sidebar</p>
+      <div>
+        <Form role="set-preferences">
+          <select id="category" name="category" onChange={handleSelectCategory}>
+            {options}
+          </select>
+        </Form>
+      </div>
     </div>
   );
 }
