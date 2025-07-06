292c860be798a1a8c06205017400cfe7124c3661
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-07-06 17:46:33 +0500

    Product information is being pulled correctly based on the product id passed to the product page. Simple template without styling being shown at the moment.

--- src/routes/ProductPage/ProductPage.jsx
+++ src/routes/ProductPage/ProductPage.jsx
@@ -1,7 +1,30 @@
-export async function loader({params}){
-  console.log(params.productId)
+import { useLoaderData } from "react-router-dom";
+
+export async function loader({ params }) {
+  const productData = await fetch(
+    `https://fakestoreapi.com/products/${params.productId}`
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(`Failed to retrieve product.\nStatus ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => data)
+    .catch((error) => {
+      throw Error(`Failed to retrieve product.\nError: ${error}`);
+    });
+  return { productData };
 }
 
 export default function ProductPage({}) {
-  return <>TODO: Build a rocket</>
+  const { productData } = useLoaderData();
+  return (
+    <>
+      <img src={productData.image} />
+      <p>{productData.title}</p>
+      <p>Rating {productData.rating.rate}</p>
+      <p>${productData.price}</p>
+    </>
+  );
 }
