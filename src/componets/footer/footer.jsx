3f349f968d8ce2bbfdab0e7f84c881bdfec88573
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-07-08 18:17:24 +0500

    Filled out and styled the footer.

--- src/componets/footer/footer.jsx
+++ src/componets/footer/footer.jsx
@@ -1,8 +1,31 @@
-import styles from  "./footer.module.css"
+import styles from "./footer.module.css";
 export default function Footer({}) {
   return (
-    <div className={styles.container}>
-      <p>Footer</p>
-    </div>
+    <>
+      <div className={styles.container}>
+        <div className={styles.horizontalContainer}>
+          <h1>Fake Store</h1>
+          <div>
+            <h4>About Us</h4>
+            <p>Blog</p>
+            <p>Careers</p>
+            <p>Sell your items</p>
+          </div>
+          <div>
+            <h4>Support</h4>
+            <p>Help</p>
+            <p>Delivery issues</p>
+            <p>Product help</p>
+          </div>
+          <div>
+            <h4>Community</h4>
+            <p>Manufacturers</p>
+            <p>Designers</p>
+            <p>Testimonials</p>
+          </div>
+        </div>
+        <p>© Copyright 2025 -Not a real company</p>
+      </div>
+    </>
   );
 }
