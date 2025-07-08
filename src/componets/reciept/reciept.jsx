afaa6baa43dec709c09d0436ba74bfe2bff19d80
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-07-08 16:45:10 +0500

    Added conditional render to receipt to not calculate total if cart is empty.

--- src/componets/reciept/reciept.jsx
+++ src/componets/reciept/reciept.jsx
@@ -1,26 +1,40 @@
+import { Link } from "react-router-dom";
 import styles from "./reciept.module.css";
 
 export default function Reciept({ className, totalPrice }) {
   return (
     <div className={`${className} ${styles.container}`}>
-      <h2 style={{textAlign: "center"}}>Cart Overview</h2>
-      <div className={styles.horizontalContainer}>
-        <p>Products total </p> <p>${totalPrice}</p>
-      </div>
-      <div className={styles.horizontalContainer}>
-        <p>Tax (6% sales tax) </p> <p> +${(totalPrice*0.06).toFixed(2)}</p>
-      </div>
-      <div className={styles.horizontalContainer}>
-        <p>Shipping </p> <p>$10.99</p>
-      </div>
-      <div className={styles.horizontalContainer}>
-        <p>Estimated delivery </p> <p>2-3 weeks</p>
-      </div>
-      <hr />
-      <div className={styles.horizontalContainer}>
-        <h3>Total </h3> <p>${(totalPrice*1.06+10.99).toFixed(2)}</p>
-      </div>
-      <button className={styles.button}>Place Order</button>
+      {totalPrice > 0 ? (
+        <>
+          <h2 style={{ textAlign: "center" }}>Cart Overview</h2>
+          <div className={styles.horizontalContainer}>
+            <p>Products total </p> <p>${totalPrice}</p>
+          </div>
+          <div className={styles.horizontalContainer}>
+            <p>Tax (6% sales tax) </p>{" "}
+            <p> +${(totalPrice * 0.06).toFixed(2)}</p>
+          </div>
+          <div className={styles.horizontalContainer}>
+            <p>Shipping </p> <p>$10.99</p>
+          </div>
+          <div className={styles.horizontalContainer}>
+            <p>Estimated delivery </p> <p>2-3 weeks</p>
+          </div>
+          <hr />
+          <div className={styles.horizontalContainer}>
+            <h3>Total </h3> <p>${(totalPrice * 1.06 + 10.99).toFixed(2)}</p>
+          </div>
+          <button className={styles.button}>Place Order</button>
+        </>
+      ) : (
+        <>
+          <h2 style={{ textAlign: "center" }}>Cart Overview</h2>
+          <p>No items in cart!</p>
+          <Link to="/store"><button className={styles.button}>
+            Check out products
+          </button></Link>
+        </>
+      )}
     </div>
   );
 }
