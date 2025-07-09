e529a621fb9774d9273a8aa522ee28e2317901da
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-07-09 19:10:26 +0500

    Added histroy tracking to the UI of Landing Page.

--- src/routes/LandingPage/LandingPage.jsx
+++ src/routes/LandingPage/LandingPage.jsx
@@ -1,6 +1,21 @@
+import ProductCard from "../../componets/productCard/productCard";
 import styles from "./LandingPage.module.css";
 
 export default function LandingPage({}) {
+  let history = sessionStorage.getItem("history");
+  history = history ? JSON.parse(history) : { products: [] };
+
+  const productsView = [];
+  for (let i = 0; i < history.products.length; i++) {
+    console.log(history.products[i]);
+    productsView.push(
+      <ProductCard
+        key={history.products[i].id}
+        productData={history.products[i]}
+      />
+    );
+  }
+
   return (
     <>
       <div className={styles.container}>
@@ -10,6 +25,11 @@ export default function LandingPage({}) {
       </div>
       <div className={styles.content}>
         <h1 tabIndex="0">Previously Viewed Products</h1>
+        {productsView.length > 0 ? (
+          <div className={styles.horizontalContainer}>{productsView}</div>
+        ): (
+          <p tabIndex="0">You haven't looked at any of our AWESOME products yet!</p>
+        )}
       </div>
     </>
   );
